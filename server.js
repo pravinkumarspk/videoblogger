1001d5a885c52ab9c6c1b691200a5ddc033ae7db
pravinkumarspk <153749620+pravinkumarspk@users.noreply.github.com>
2024-12-03 19:35:49 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
