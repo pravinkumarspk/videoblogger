6c68ae87b8623f4a788f7640930fd0c114d39f91
pravinkumarspk <153749620+pravinkumarspk@users.noreply.github.com>
2024-12-03 19:58:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -91,14 +91,11 @@ app.post('/create-blog', async (req, res) => {
   }
 });
 
-app.use(express.static(path.join(__dirname, 'build')));
 
 
 
 //he is the main one
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
